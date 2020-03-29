785ab16822934816ab82e2593851aadee7dca4be
Herman Polderman <herman@polderman.net>
2020-03-29 17:41:44 +0200

    ontdubbel den Hoorn

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,4 +1,4 @@
 git pull
 sudo  docker-compose run --rm hugo -b https://westlandsemolens.polderman.net
 #-d /var/www/westlandsemolens.polderman.net/"
-sync -avz --delete public/ /var/www/westlandsemolens.polderman.net/
+rsync -avz --delete public/ /var/www/westlandsemolens.polderman.net/
