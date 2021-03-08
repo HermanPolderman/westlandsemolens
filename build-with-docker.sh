7ca108e5d184328ed0a5e847825f3083e1fef7c5
Herman Polderman <herman@polderman.net>
2021-03-08 23:34:01 +0100

    robots index follow, images

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,4 +1,3 @@
 git pull
-sudo  docker-compose run --rm hugo -b https://www.westlandsemolens.nl
-#-d /var/www/westlandsemolens.polderman.net/"
-rsync -avz --delete public/ /var/www/www.westlandsemolens.nl/
+sudo  docker-compose run --rm hugo -b https://www.westlandsemolens.nl/
+sudo rsync -avz --delete public/ /var/www/www.westlandsemolens.nl/
