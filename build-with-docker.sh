885281b4abae4337338fa1d134537a3dc4f908cb
herman <herman@vps-hermanpolderman.polderman.net>
2021-04-05 16:35:25 +0200

    buildwithdocker

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,3 +1,5 @@
 git pull
 sudo  docker-compose run --rm hugo -e production -b https://www.westlandsemolens.nl/
-sudo rsync -avz --delete public/ /var/www/www.westlandsemolens.nl/
+sudo chown -R herman *
+sudo chgrp -R herman *
+rsync -avz --delete public/ /var/www/www.westlandsemolens.nl/
