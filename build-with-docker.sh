1b44b2494c10914333122c309ea845c3c84f2fac
herman <herman@vps-hermanpolderman.polderman.net>
2021-04-05 14:56:53 +0200

    always vuild with production flag

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,3 +1,3 @@
 git pull
-sudo  docker-compose run --rm hugo -b https://www.westlandsemolens.nl/
+sudo  docker-compose run --rm hugo -e production -b https://www.westlandsemolens.nl/
 sudo rsync -avz --delete public/ /var/www/www.westlandsemolens.nl/
